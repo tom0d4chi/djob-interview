1b4c7be0204370afc6c238da160c0cb7c2099dff
tom0d4chi <tomod4chi@proton.me>
2024-08-23 10:58:50 +0200

    Style select

--- src/components/SelectCategory.tsx
+++ src/components/SelectCategory.tsx
@@ -2,11 +2,13 @@ import { SelectCategoryProps } from "../types"
 
 export default function SelectCategory({categories, handleCategoryChange}: SelectCategoryProps){
     return (
-        <select name="category" onChange={e => handleCategoryChange(e.target.value)}>
+        <div className="rounded-full h-11 my-4 border-[#636363] border-[1px] w-72 px-4">
+        <select className="h-full px-4 w-full bg-[#1e1e1e] border-0  text-white pl-0" name="category" onChange={e => handleCategoryChange(e.target.value)}>
             {categories.map(category => 
                 <option key={category} value={category}>
                     {category}
                 </option>)}
         </select>
+        </div>
     )
 }
